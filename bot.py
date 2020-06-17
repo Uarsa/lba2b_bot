f3aa78baf9d12bb0fb90581c85f70295d7253544
Uarsa <59147934+Uarsa@users.noreply.github.com>
2020-06-17 18:22:38 +0300

    if type(message.text) == type(1):

--- bot.py
+++ bot.py
@@ -7,17 +7,26 @@ bot = telebot.TeleBot("1174476779:AAE0zX3xf-ioir4agNdGiBHQFjObQZJBcqA")
 @bot.message_handler(commands=['start'])
 def handle_start(message):
         user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
-        user_markup.row('/start','/rol')
+        user_markup.row('/start')
         bot.send_message(message.from_user.id, "Давай начнём.\nПосто пришли номер блока.", reply_markup=user_markup)
         
         
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+        if type(message.text) == type(1):
+                i = int(message.text) * 562
+                bot.send_message(message.chat.id, i)
+        else:
+                echo_all(message)
+                
+        
+        '''
          if message.text == 'пока' or message.text == 'Пока':
                 bot.send_message(message.chat.id, 'Успехов в новом году!')
          else:
                 i = int(message.text) * 562.5
                 bot.send_message(message.chat.id, i)
+         '''       
         
         
 bot.polling(none_stop=True, interval=0)    
