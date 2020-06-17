0123a74dc91ae184e7e2756f875cf14c540df9f5
Uarsa <59147934+Uarsa@users.noreply.github.com>
2020-06-17 16:06:01 +0300

    Create bot.

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
-print("!")
+import requests
+import telebot
+from telebot import apihelper
+from telebot.types import Message
+
+
+bot = telebot.TeleBot("1174476779:AAE0zX3xf-ioir4agNdGiBHQFjObQZJBcqA")
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+        user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
+        user_markup.row('/start','/rol')
+        bot.send_message(message.from_user.id, "Давай начнём.\nПосто пришли номер блока.", reply_markup=user_markup)
+        
+        
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+         if message.text == 'пока' or message.text == 'Пока':
+                bot.send_message(message.chat.id, 'Успехов в новом году!\nЗаходи ещё, позже у меня будет больше возможностей с:'))
+        
+        
+bot.polling(none_stop=True, interval=0)    
